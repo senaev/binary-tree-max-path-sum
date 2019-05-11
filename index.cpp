1fbbc6f7382bc985771c4f85f72d9f92ba88d80f
Andrey Senaev <senaev@yandex-team.ru>
2019-05-12 02:06:19 +0300

    complete

--- index.cpp
+++ index.cpp
@@ -108,6 +108,7 @@ void visualizeTree(TreeNode* root) {
         TreeNode* node = getNodeByIndex(root, i);
 
         if (!node) {
+            cout << endl;
             return;
         }
 
@@ -131,11 +132,166 @@ void visualizeTree(TreeNode* root) {
     }
 }
 
+tuple<int, int> maxForTree(TreeNode* root, int currentMaximum) {
+    const int val = root -> val;
+
+    if (!root -> left && !root -> right) {
+        return {val, val};
+    }
+
+    if (!root -> left || !root -> right) {
+        TreeNode* branchNode = root -> left
+            ? root -> left
+            : root -> right;
+
+        auto [branchPathMax, branchMax] = maxForTree(branchNode, currentMaximum);
+
+        int sum = val + branchMax;
+        int maxPath = max({val, sum, branchPathMax});
+        int maxBranch = max({val, sum});
+
+        return {maxPath, maxBranch};
+    }
+
+    int leftMax;
+    int leftBranchMax;
+    int rightMax;
+    int rightBranchMax;
+
+    if (root -> left) {
+        auto [_leftMax, _leftBranchMax] = maxForTree(root -> left, currentMaximum);
+        leftMax = _leftMax;
+        leftBranchMax = _leftBranchMax;
+    }
+
+    if (root -> right) {
+        auto [_rightMax, _rightBranchMax] = maxForTree(root -> right, currentMaximum);
+        rightMax = _rightMax;
+        rightBranchMax = _rightBranchMax;
+    }
+
+    int rootMaxPath = leftBranchMax + rightBranchMax + val;
+
+    int leftToTop = leftBranchMax + val;
+    int rightToTop = rightBranchMax + val;
+
+    int maxPath = max({val, leftMax, rightMax, leftToTop, rightToTop, rootMaxPath});
+    int branchMax = max({val, leftToTop, rightToTop});
+
+    return { maxPath, branchMax };
+}
+
 int main()
 {
-    vector<int> treeMap { -10, 9, 20, 0, 0, 15 , 7 };
+    vector<int> treeMap { 
+                      0,
+          1,                    0, 
+  1,            1,          0,        0, 
+0, -1,        0, 0,       0, 0,      0, 0
+         };
+    TreeNode* root1 = constructTree(treeMap);
+    auto [firstMax, zero] = maxForTree(root1, INT_MIN);
+    if (firstMax != 3) {
+        cout << "!!!!!!!!!!!!!!!!!!!!!first error " << firstMax << endl;
+        throw 0;
+    }
 
-    TreeNode* root = constructTree(treeMap);
+    vector<int> treeMap2 {
+                             0,
+            1,                                0,
+      1,              1,           0,                   2,
+   0,    -1,         0,    0,     0,    0,            0,       0,
+};
 
-    visualizeTree(root);
+    TreeNode* root2 = constructTree(treeMap2);
+    auto [secondMax, zero2] = maxForTree(root2, INT_MIN);
+    if (secondMax != 4) {
+        cout << "!!!!!!!!!!!!!!!!!!!!!second error" << endl;
+        throw 0;
+    }
+
+    vector<int> treeMap3 {1, 2, 3};
+
+    TreeNode* root3 = constructTree(treeMap3);
+    auto [thirdMax, zero3] = maxForTree(root3, INT_MIN);
+    if (thirdMax != 6) {
+        cout << "!!!!!!!!!!!!!!!!!!!!!third error" << endl;
+        throw 0;
+    }
+
+    vector<int> treeMap4 {-2, -1};
+
+    TreeNode* root4 = constructTree(treeMap4);
+    auto [fourthMax, zero4] = maxForTree(root4, INT_MIN);
+    if (fourthMax != -1) {
+        cout << "!!!!!!!!!!!!!!!!!!!!!forth error" << endl;
+        throw 0;
+    }
+
+    vector<int> treeMap5 {1, 2};
+
+    TreeNode* root5 = constructTree(treeMap5);
+    auto [fifthMax, zero5] = maxForTree(root5, INT_MIN);
+    if (fifthMax != 3) {
+        cout << "!!!!!!!!!!!!!!!!!!!!!fifth error" << endl;
+        throw 0;
+    }
+
+    vector<int> treeMap6 {2, -1};
+
+    TreeNode* root6 = constructTree(treeMap6);
+    auto [sixthMax, zero6] = maxForTree(root6, INT_MIN);
+    if (sixthMax != 2) {
+        cout << "!!!!!!!!!!!!!!!!!!!!!sixthMax error" << endl;
+        throw 0;
+    }
+
+    vector<int> treeMap7 {
+                5,
+         4,            8,
+    11,      0,     13,     4,
+   7, 2,   0,  0,  0, 0,  0,   1
+   };
+
+    TreeNode* root7 = constructTree(treeMap7);
+    auto [seventhMax, zero7] = maxForTree(root7, INT_MIN);
+    if (seventhMax != 48) {
+        cout << "!!!!!!!!!!!!!!!!!!!!!seventhMax error " << seventhMax << endl;
+        throw 0;
+    }
+
+    vector<int> treeMap8 {1,-2,3};
+
+    TreeNode* root8 = constructTree(treeMap8);
+    auto [eighthMax, zero8] = maxForTree(root8, INT_MIN);
+    if (eighthMax != 4) {
+        cout << "!!!!!!!!!!!!!!!!!!!!!eighthMax error " << eighthMax << endl;
+        throw 0;
+    }
+
+    vector<int> treeMap9 {2,-1, -2};
+
+    TreeNode* root9 = constructTree(treeMap9);
+    auto [ninthMax, zero9] = maxForTree(root9, INT_MIN);
+    if (ninthMax != 2) {
+        cout << "!!!!!!!!!!!!!!!!!!!!!ninthMax error " << ninthMax << endl;
+        throw 0;
+    }
+
+    vector<int> treeMap10 {
+                                    -1,
+                5,                                      0,
+         4,              0,                         0,          0,
+     0,         2,     0,     0,            0,           0,     0,   0,
+0,      0,    -4
+};
+
+    TreeNode* root10 = constructTree(treeMap10);
+    auto [tenthMax, zero10] = maxForTree(root10, INT_MIN);
+    if (tenthMax != 11) {
+        cout << "!!!!!!!!!!!!!!!!!!!!!tenthMax error " << tenthMax << endl;
+        throw 0;
+    }
+
+    cout << tenthMax << endl;
 }
